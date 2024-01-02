dfcaa245b368e52e3e4ad581e49068c80d71648d
Matthew <dydals3440@gmail.com>
2024-01-02 20:49:00 +0900

    feat: Create User by Neon Prisma DB

--- actions/register.ts
+++ actions/register.ts
@@ -1,9 +1,11 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
 import { RegisterSchema } from '@/app/schemas';
-
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 // our server code never be bundle client
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -13,5 +15,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: 'Invalid Fields!' };
   }
-  return { success: 'Email sent!' };
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      // salt화 시킨 패스워드 넣는게 핵심!
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send Verification token email
+
+  return { success: 'User Created!' };
 };
